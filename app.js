c67602bdde30cf539b4691613d75a26ac10a4388
edswartz <edswartz@yahoo.com>
2016-03-30 19:59:55 -0400

    add click event

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-$("hero").click(function() {
+$("#hero").click(function() {
     var fontSize = parseInt($(this).css("font-size"));
     fontSize = fontSize + 1 + "px";
-    $('hero').css({'font-size':fontSize});
+    $('#hero').css({'font-size':fontSize});
 });
